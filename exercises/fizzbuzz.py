ff1e1d074f49ec8448b5a67441aadb4d15bf84c2
Mylene Hortaleza <mylene@myleneh.com>
2015-05-17 22:37:22 -0700

    FizzBuzz

---
+++ exercises/fizzbuzz.py
@@ -0,0 +1,19 @@
+"""
+Write a program that prints the numbers from 1 to 100. 
+But for multiples of three print “Fizz” instead of the number and 
+for the multiples of five print “Buzz”. 
+For numbers which are multiples of both three and five print “FizzBuzz”.
+
+http://imranontech.com/2007/01/24/using-fizzbuzz-to-find-developers-who-grok-coding/
+"""
+
+for i in range(1, 100):
+    output = ""
+    if i % 3 == 0:
+        output = output + "Fizz"
+    if i % 5 == 0:
+        output = output + "Buzz"
+    if not output:
+        output = "{0}".format(i)
+    print(output)
+
